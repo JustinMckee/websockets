1fb6b360fc975b592dd969ad4239095512d6d63e
Justin McKee <thisisjustinmckee@gmail.com>
2023-10-02 14:18:07 -0400

    Adds simple websockets exampled

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import {WebSocketServer} from "ws";
+
+const wss = new WebSocketServer({port: 8082});
+
+wss.on("connection", ws => {
+    console.log("New client connected!");
+
+    ws.on("message", data => {
+        console.log(`Client has sent us: ${data}`);
+        ws.send(data.toString().toUpperCase());
+    })
+
+    ws.on("close", () => {
+        console.log("Client has disconnected.");
+    });
+});
